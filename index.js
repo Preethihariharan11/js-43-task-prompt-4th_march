ada0d21a0715477ee3dec52ff5d55d20c03e5d37
Preethihariharan11 <preethihariit03@gmail.com>
2020-03-04 17:42:08 +0530

    1 file modified

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
-// Import stylesheets
-import './style.css';
-
-// Write Javascript code!
-const appDiv = document.getElementById('app');
-appDiv.innerHTML = `<h1>JS Starter</h1>`;
+let person=prompt("please Enter your name");
+let start=prompt("Start index");
+while(isNaN(start)==true)
+{
+  alert("Enter valid value");
+  let start=prompt("start index");
+}
+let end=prompt("end index");  
+while(isNaN(end)==true)
+{
+  alert("Enter valid value");
+  let end=prompt("End index");
+}
+alert(person.slice(start,end));
